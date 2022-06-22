1207bc2a0ce86ea9f650009d8358ad75d7b45e57
MaxRaphael <maxmelamed4@gmail.com>
2022-06-22 18:53:45 +1000

    yaya\

--- flask_app.py
+++ flask_app.py
@@ -7,15 +7,17 @@ app = Flask(__name__)
  
 @app.route('/form')
 def form():
-    return render_template('form.html')
+    dir = "/home/MaxRaphael/raphaPIE/form.html"
+    return render_template(dir)
  
 @app.route('/data/', methods = ['POST', 'GET'])
 def data():
+    dir = "/home/MaxRaphael/raphaPIE/data.html"
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         form_data = request.form
-        return render_template('data.html',form_data = form_data)
+        return render_template(dir,form_data = form_data)
 
 if __name__ == '__main__':
     app.run()
