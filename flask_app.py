b870a2ac8b8aaf8094a6f9ddb3d4ea80d4d75c89
MaxRaphael <maxmelamed4@gmail.com>
2022-06-22 20:33:24 +1000

    ha

--- flask_app.py
+++ flask_app.py
@@ -8,10 +8,17 @@ import sys
 app = Flask(__name__)
  
  
-@app.route("/")
-def blogs():
-    path = "/home/MaxRaphael/raphaPIE/data.html"
-    return render_template(path)
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
 
 if __name__ == '__main__':
     app.run()
