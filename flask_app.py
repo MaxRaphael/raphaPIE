98d74ca3211137be6adcee50f1027184fe1092ab
MaxRaphael <maxmelamed4@gmail.com>
2022-06-22 18:57:59 +1000

    p

--- flask_app.py
+++ flask_app.py
@@ -5,19 +5,11 @@ from rhino3dm import *
  
 app = Flask(__name__)
  
-@app.route('/form')
-def form():
+ 
+@app.route("/")
+def blogs():
     dir = "/home/MaxRaphael/raphaPIE/form.html"
     return render_template(dir)
- 
-@app.route('/data/', methods = ['POST', 'GET'])
-def data():
-    dir = "/home/MaxRaphael/raphaPIE/data.html"
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template(dir,form_data = form_data)
 
 if __name__ == '__main__':
     app.run()
