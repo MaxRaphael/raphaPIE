a0b344e908fcc84fc130636fc7283550b6af8439
MaxRaphael <maxmelamed4@gmail.com>
2022-06-23 18:59:16 +1000

    hi

--- flask_app.py
+++ flask_app.py
@@ -1,28 +1,25 @@
-
-from flask import Flask,render_template,request
-from flask_app import app 
-from rhino3dm import *
-
- 
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+import ghhops_server as hs
 app = Flask(__name__)
- 
- 
-@app.route('/')
-def form():
-    return render_template('form.html')
- 
-@app.route('/data', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
+hops = hs.Hops(app)
 
+@hops.component(
+    "/cv_ShapeIdentifier",
+    name="cv_ShapeIdentifier",
+    description="This tool identifies a shape and then draws it into grasshopper",
+    inputs=[
+        hs.HopsNumber("Shape"),
+    ],
+    outputs=[hs.HopsPoint("Output")]
+)
+
+@app.route('/')
+def cv_ShapeIdentifier():
+    return "hi"
+    
+
+   
 if __name__ == '__main__':
     app.run()
- 
-
-
-
-
